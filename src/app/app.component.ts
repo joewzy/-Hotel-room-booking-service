99dbe3f1b59f50c93b2394eac92271ad7aa790b0
joewzy <josiahkotey13@gmail.com>
2024-11-13 00:55:44 +0000

    user register check for non-empty fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,LoginComponent,RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
